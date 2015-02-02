c59c99852e68c65a28408a61fde44cf48b2f4ace
Losses Don <losses.don@gmail.com>
2015-02-02 14:10:49 +0800

    增强型开发人员调试工具

--- script/development.js
+++ script/development.js
@@ -2,6 +2,19 @@
  * Created by Don on 1/25/2015.
  */
 //打开开发人员窗口
-$(document).bind('keydown', 'f12', function(evt) {
+$(document).bind('keydown', 'f12', function (evt) {
     win.showDevTools();
+});
+
+$(document).bind('keydown', 'f5', function (evt) {
+    history.go(0);
+});
+
+$(document).bind('keydown', 'f6', function (evt) {
+    Adolfans.openFile("../transformtest");
+});
+
+$(document).bind('keydown', 'f7', function (evt) {
+    try{regex.HTMLToMarkdown(document.querySelector("#main_area"));}
+    catch(e){console.log(e)};
 });
